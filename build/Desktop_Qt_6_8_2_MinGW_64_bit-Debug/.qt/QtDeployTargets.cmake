14d3616bbd0714421dbe1450c528c7c3508c399c
Uumutunal <unal.umut98@gmail.com>
2025-02-23 01:05:51 +0300

    Removed build files from version control

--- build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_texture2_FILE C:/Users/Administrator/Desktop/Python/qt/texture2/build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/texture2.exe)
-set(__QT_DEPLOY_TARGET_texture2_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_texture2_RUNTIME_DLLS C:/Qt/6.8.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.2/mingw_64/bin/Qt6Core.dll)
